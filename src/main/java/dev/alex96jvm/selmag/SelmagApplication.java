733f6448cbe8ba59fad442d1591f59753584fcb4
alex96jvm <alex96jvm@gmail.com>
2024-08-27 12:57:44 +0300

    Init commit

---
+++ src/main/java/dev/alex96jvm/selmag/SelmagApplication.java
@@ -0,0 +1,13 @@
+package dev.alex96jvm.selmag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelmagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alex96jvm/selmag/SelmagApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alex96jvm.selmag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelmagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
