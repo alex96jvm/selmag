77d741e2a98436cbe0ab1007ab06b16e03f3c7e5
alex96jvm <alex96jvm@gmail.com>
2024-08-29 14:24:29 +0300

    Add product not found (404) exception handler

--- src/main/java/dev/alex96jvm/selmag/manager/controller/ProductController.java
+++ src/main/java/dev/alex96jvm/selmag/manager/controller/ProductController.java
@@ -4,10 +4,14 @@ import dev.alex96jvm.selmag.manager.controller.payload.NewProductPayload;
 import dev.alex96jvm.selmag.manager.controller.payload.UpdateProductPayload;
 import dev.alex96jvm.selmag.manager.entity.Product;
 import dev.alex96jvm.selmag.manager.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("catalogue/products/{productId:\\d+}")
@@ -18,7 +22,8 @@ public class ProductController {
 
     @ModelAttribute("product")
     public Product product(@PathVariable("productId") int productId){
-        return this.productService.findProduct(productId).orElseThrow();
+        return this.productService.findProduct(productId)
+                .orElseThrow(() -> new NoSuchElementException("Товар не найден"));
     }
 
         @GetMapping()
@@ -37,9 +42,19 @@ public class ProductController {
         this.productService.updateProduct(product.getId(), payload.title(), payload.details());
         return "redirect:/catalogue/products/%d".formatted(product.getId());
     }
+
     @PostMapping("delete")
     public String deleteProduct(@ModelAttribute("product") Product product){
         this.productService.deleteProduct(product.getId());
         return "redirect:/catalogue/products/list";
     }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String noSuchElementException(NoSuchElementException e, Model model,
+                                         HttpServletResponse response){
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error", e.getMessage());
+        return "errors/404";
+    }
+
 }
