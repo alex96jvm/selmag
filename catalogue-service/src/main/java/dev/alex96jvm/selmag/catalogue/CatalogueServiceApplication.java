fab254947c86223c4ea1ea994cf7bc3164386ed3
alex96jvm <alex96jvm@gmail.com>
2024-09-19 14:47:09 +0300

    Add SpringBootCatalogueServiceApplication

---
+++ catalogue-service/src/main/java/dev/alex96jvm/selmag/catalogue/CatalogueServiceApplication.java
@@ -0,0 +1,11 @@
+package dev.alex96jvm.selmag.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
+}
--- manager-app/src/main/java/dev/alex96jvm/selmag/manager/ManagerApplication.java
+++ manager-app/src/main/java/dev/alex96jvm/selmag/manager/ManagerApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 	ManagerApplication {
-
+public class ManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
-
 }
